de747b5a8775af4b4c34685c9bfce3736cfd7b82
fanyao <fanyaoyao12138@163.com>
2021-05-08 17:23:20 +0800

    feat(添加跨域):

--- src/main/java/com/cebon/jiwei/shengxunzhuji/config/WebMvcConfig.java
+++ src/main/java/com/cebon/jiwei/shengxunzhuji/config/WebMvcConfig.java
@@ -6,6 +6,7 @@ import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.ArrayList;
@@ -14,10 +15,10 @@ import java.util.List;
 /**
  * @author: bugProvider
  * @date: 2021/5/8 13:26
- * @description: 消息转换器
+ * @description:
  */
 @Configuration
-public class MessageConvertersConfigure implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     FastJsonHttpMessageConverter fastJsonHttpMessageConverter() {
         FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
@@ -43,4 +44,17 @@ public class MessageConvertersConfigure implements WebMvcConfigurer {
         converters.clear();
         converters.add(fastJsonHttpMessageConverter());
     }
+
+    static final String ORIGINS[] = new String[] { "GET", "POST", "PUT", "DELETE" };
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 所有的当前站点的请求地址，都支持跨域访问。
+                .allowedOrigins("*") // 所有的外部域都可跨域访问。 如果是localhost则很难配置，因为在跨域请求的时候，外部域的解析可能是localhost、127.0.0.1、主机名
+                .allowCredentials(true) // 是否支持跨域用户凭证
+                .allowedMethods(ORIGINS) // 当前站点支持的跨域请求类型是什么
+                .maxAge(3600); // 超时时长设置为1小时。 时间单位是秒。
+    }
+
 }
+
